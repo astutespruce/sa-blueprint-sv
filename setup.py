6e45c337c158c3a5665a808ab3034b8cef1d4036
Brendan C. Ward <bcward@astutespruce.com>
2020-09-18 10:50:58 -0700

    Fix corridors text in report and setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ import os
 from setuptools import setup
 
 
+description = "South Atlantic Blueprint 2020 Simple Viewer and Custom Reporting"
+
+if os.path.exists("README.md"):
+    long_description = open("README.md").read()
+else:
+    long_description = description
+
 setup(
-    name="sa-blueprint-sv",
+    name="South Atlantic Blueprint 2020 Simple Viewer",
     version="0.5.0",
     url="https://github.com/astutespruce/sa-blueprint-sv",
     license="MIT",
     author="Brendan C. Ward",
     author_email="bcward@astutespruce.com",
-    description="Custom reporting for South Atlantic Conservation Blueprint",
+    description=description,
     long_description_content_type="text/markdown",
-    long_description=open("README.md").read(),
-    # see Pipfile
+    long_description=long_description,
     install_requires=[],
 )
