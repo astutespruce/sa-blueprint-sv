1e4d73fc5a52e5f44e5e9756e774c767e07f1eaa
Brendan C. Ward <bcward@astutespruce.com>
2020-10-08 12:00:22 -0700

    Add submit of user info to MS form, use react-hook-form

--- ui/src/components/report/api.js
+++ ui/src/components/report/api.js
@@ -1,4 +1,4 @@
-import { hasWindow } from 'util/dom'
+import { hasWindow, saveToStorage, encodeParams } from 'util/dom'
 import { captureException } from 'util/log'
 import config from '../../../gatsby-config'
 
@@ -133,3 +133,90 @@ const pollJob = async (jobId, onProgress) => {
       'timeout while creating report.  Your area of interest may be too big.',
   }
 }
+
+export const submitUserInfo = async (userInfo) => {
+  const { userEmail, userName, userOrg, userUse } = userInfo
+  console.log('submit user info', userEmail, userName, userOrg, userUse)
+
+  const formURL = `https://forms.office.com/formapi/api/0693b5ba-4b18-4d7b-9341-f32f400a5494/users/765228b1-d0d0-4438-812e-51cbb57819f1/forms('urWTBhhLe02TQfMvQApUlLEoUnbQ0DhEgS5Ry7V4GfFURFMxWkczNDM4NkFPNloySTBHMjhXVVZIWC4u')/responses`
+  const questionIds = {
+    userEmail: 'r792746f558e844148724fa5fcfec95fc',
+  }
+
+  const answers = Object.entries(questionIds).map(([field, questionId]) => ({
+    questionId,
+    answer1: userInfo[field],
+  }))
+
+  try {
+    // in no-cors mode, we can submit but not receive content
+    await fetch(formURL, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: encodeParams({ answers: JSON.stringify(answers) }),
+    })
+
+    saveToStorage('reportForm', userInfo)
+  } catch (ex) {
+    console.error('Could not submit user info to MS Form', userInfo)
+  }
+}
+
+//
+
+// import { siteMetadata } from '../../../gatsby-config'
+
+// const {
+//   mailchimpConfig: { userID, formID, formURL },
+// } = siteMetadata
+
+// if (!(formURL && userID && formID)) {
+//   console.error('Mail chimp form env vars need to be provided in .env file')
+// }
+
+// Note: we have to add -json to the end of the regular form URL, make sure it is formatted correctly in .env.*
+// fetchJSONP(
+//   `${formURL}-json?${encodeParams({
+//     u: userID,
+//     id: formID,
+//     ...data,
+//   })}`,
+//   {
+//     jsonpCallback: 'c',
+//   }
+// )
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(({ result, msg }) => {
+//     console.log('json', result, msg)
+//     if (result === 'error') {
+//       if (msg.search('already subscribed') !== -1) {
+//         // this is an error ot Mailchimp, but not a problem for us
+//         saveToStorage('downloadForm', data)
+//         setState({ isPending: false, isError: false, isSuccess: true })
+
+//         onContinue()
+//       } else {
+//         setState({ isPending: false, isError: true })
+//         // TODO: report error to sentry
+//       }
+//     } else {
+//       // assume it was successful
+//       saveToStorage('downloadForm', data)
+//       setState({ isPending: false, isError: false, isSuccess: true })
+//       // TODO:
+
+//       onContinue()
+//     }
+//   })
+//   .catch(error => {
+//     console.error(error)
+//     setState({ isPending: false, isError: true })
+
+//     // TODO: report error to sentry
+//   })
+// }
